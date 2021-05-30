b8ceee5643f793c305774592bfbdaaacca4dc7a5
Bhagwan.Singh <Bhagwan.Singh@SYPULJXZT503.synechron.com>
2021-05-30 15:38:25 +0530

    demo

---
+++ day02-microservice/src/main/java/com/api/syne/Day02MicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.api.syne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day02MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day02MicroserviceApplication.class, args);
+	}
+
+}
